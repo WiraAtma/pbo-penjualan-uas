c32dff48ac28764896cac773a61b514fca94d590
IvanMarcel <2401020023@primakara.ac.id>
2025-12-22 20:17:53 +0800

    Sale

--- src/Models/Sale.java
+++ src/Models/Sale.java
@@ -2,21 +2,34 @@ package Models;
 
 public class Sale {
     private int id;
-    // lanjut
+    private String productName;
+    private int qty;
+    private double price;
+    private double total;
+
 
     // buat constructur
-    public Sale(int id) { // tambah lainnya
-        // isi validate disini
+    public Sale(int id, String productName, int qty, double price, double total) { // tambah lainnya
+        this.id = id;
+        this.productName = productName;
+        this.qty = qty;
+        this.total = total;
+        this.price = price;
+
     }
 
     public int getId() {
         return id;
     }
+    public String getProductName() {return productName;}
+    public int getQty() {return  qty;}
+    public double getPrice() {return price;}
+    public double getTotal() {return total;}
 
-    // lanjutkan
+
 
     // buat return untuk ke file
     public String toFileString() {
-        return ""; // isi disini contoh : id + "," + kode + ...
+        return id + "," + productName + "," + qty + "," + price + "," + total ;
     }
 }
